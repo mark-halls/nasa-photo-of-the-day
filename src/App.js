e01e6cb138737af18ec69267aa1fe2b8b934fcbd
Mark Halls <mark.halls@gmail.com>
2019-10-09 16:10:30 -0700

    Broken nested objects

--- src/App.js
+++ src/App.js
@@ -9,50 +9,52 @@ import PreviousImages from "./components/PreviousImages";
 
 import "./App.css";
 
-const myKey = "DEMO_KEY";
+// const myKey = "DEMO_KEY";
 
 function App() {
   // 7 days worth of image data
   const [images, setImages] = useState({});
   const [imageIndex, setImageIndex] = useState("");
 
-  // useEffect(() => {
-  //   const setImageState = async () => {
-  //     const data = {};
-
-  //     // add 7 days worth of empty objects
-  //     for (let i = 0; i < 7; i++) {
-  //       const day = moment()
-  //         .subtract(i, "days")
-  //         .format("YYYY-MM-DD");
-  //       data[day] = {};
-  //     }
-
-  //     const index = Object.keys(data)
-  //       .sort()
-  //       .reverse()[0];
-
-  //     // loop over data to get image data from api
-  //     await Object.keys(data).forEach(day => {
-  //       axios
-  //         .get(
-  //           `https://api.nasa.gov/planetary/apod?date=${day}&api_key=${myKey}`
-  //         )
-  //         .then(res => {
-  //           data[day] = res.data;
-  //         });
-  //     });
-  //     setImages(data);
-  //     setImageIndex(index);
-  //   };
-  //   setImageState();
-  // }, []);
-
   useEffect(() => {
-    axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=${myKey}`)
-      .then(res => setImages(res.data));
+    const setImageState = async () => {
+      const data = {};
+
+      // add 7 days worth of empty objects
+      for (let i = 0; i < 7; i++) {
+        const day = moment()
+          .subtract(i, "days")
+          .format("YYYY-MM-DD");
+        data[day] = {};
+      }
+
+      // loop over data to get image data from api
+      await Object.keys(data).forEach(day => {
+        axios
+          .get(
+            `https://api.nasa.gov/planetary/apod?date=${day}&api_key=${myKey}`
+          )
+          .then(res => {
+            data[day] = res.data;
+          });
+      });
+
+      const index = Object.keys(data)
+        .sort()
+        .reverse()[0];
+
+      data["imageIndex"] = index;
+
+      setImages(data);
+    };
+    setImageState();
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://api.nasa.gov/planetary/apod?api_key=${myKey}`)
+  //     .then(res => setImages(res.data));
+  // }, []);
 
   return (
     <div className="App">
@@ -62,6 +64,7 @@ function App() {
         explanation={images.explanation}
         title={images.title}
         date={images.date}
+        test={console.log(images[images.imageIndex])}
       />
       {/* <PreviousImages images={images} /> */}
     </div>
